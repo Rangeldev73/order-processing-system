0fe47a5c355e7ce510cceec418d7ff7b041470b1
Rangel <joaquimrangel.dev@gmail.com>
2026-08-19 14:49:16 -0300

    chore(order-service): add spring-retry and spring-aspects dependencies

---
+++ order-service/src/main/java/com/rangel/orderservice/domain/model/OrderPaymentStatus.java
@@ -0,0 +1,7 @@
+package com.rangel.orderservice.domain.model;
+
+public enum PaymentStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
---
+++ order-service/src/main/java/com/rangel/orderservice/domain/model/OrderStockStatus.java
@@ -0,0 +1,7 @@
+package com.rangel.orderservice.domain.model;
+
+public enum StockStatus {
+    PENDING,
+    RESERVED,
+    FAILED
+}
